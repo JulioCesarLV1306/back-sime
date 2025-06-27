4da7508592082be1f3a22247a865be61c724be6d
JulioCesar <juliolopezvasquez13@gmail.com>
2025-06-27 16:11:26 -0500

    Eliminar método por defecto en AlumnoRepository y reafirmar métodos de búsqueda por DNI de alumno

--- src/main/java/com/sime/backwebsime/repository/AlumnoRepository.java
+++ src/main/java/com/sime/backwebsime/repository/AlumnoRepository.java
@@ -4,12 +4,10 @@ import com.sime.backwebsime.model.Alumno;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-
 import java.util.Optional;
 
 @Repository
 public interface AlumnoRepository extends JpaRepository<Alumno,Long> {
-    default boolean existsByDni(String dni) {
-        return false;
-    }
+    boolean existsByDniAlumno(String dniAlumno);
+    Optional<Alumno> findByDniAlumno(String dniAlumno);
 }

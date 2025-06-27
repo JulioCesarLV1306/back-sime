0ed18378cbd67dacc087ff2a33a5aa0da6c1be0f
JulioCesar <juliolopezvasquez13@gmail.com>
2025-06-27 16:11:36 -0500

    Eliminar método por defecto en ApoderadoRepository y reafirmar la firma del método existsByDni

--- src/main/java/com/sime/backwebsime/repository/ApoderadoRepository.java
+++ src/main/java/com/sime/backwebsime/repository/ApoderadoRepository.java
@@ -3,8 +3,9 @@ package com.sime.backwebsime.repository;
 import com.sime.backwebsime.model.Apoderado;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface ApoderadoRepository extends JpaRepository<Apoderado,Long> {
-    default boolean existsByDni(String dni) {
-        return false;
-    }
+    boolean existsByDni(String dni);
+    Optional<Apoderado> findByDni(String dni);
 }

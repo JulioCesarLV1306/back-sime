74413eadf83e0245ed91eddefad9911506dbd610
JulioCesar <juliolopezvasquez13@gmail.com>
2025-06-27 16:12:00 -0500

    Eliminar importaciones innecesarias en GradoRepository

--- src/main/java/com/sime/backwebsime/repository/GradoRepository.java
+++ src/main/java/com/sime/backwebsime/repository/GradoRepository.java
@@ -1,13 +1,9 @@
 package com.sime.backwebsime.repository;
 
-import com.sime.backwebsime.model.Aula;
 import com.sime.backwebsime.model.Grado;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
 
 @Repository
 public interface GradoRepository extends JpaRepository<Grado, Integer> {

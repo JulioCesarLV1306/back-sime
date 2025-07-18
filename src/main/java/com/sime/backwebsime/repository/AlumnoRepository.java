60acd7b63142437e0dc846d2e3c47f19881a9185
JulioCesar <juliolopezvasquez13@gmail.com>
2025-07-18 02:50:31 -0500

    Add findByEstadoAlumno method: implement repository method to retrieve students by status

--- src/main/java/com/sime/backwebsime/repository/AlumnoRepository.java
+++ src/main/java/com/sime/backwebsime/repository/AlumnoRepository.java
@@ -4,10 +4,12 @@ import com.sime.backwebsime.model.Alumno;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface AlumnoRepository extends JpaRepository<Alumno,Long> {
     boolean existsByDniAlumno(String dniAlumno);
     Optional<Alumno> findByDniAlumno(String dniAlumno);
+    List<Alumno> findByEstadoAlumno(Boolean estado);
 }
